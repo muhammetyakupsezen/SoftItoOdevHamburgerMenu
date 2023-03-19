4146de28f4209c86c971f1b0481553c68cb64702
muhammetyakupsezen <116495280+muhammetyakupsezen@users.noreply.github.com>
2023-03-19 18:03:31 +0300

    Add project files.

---
+++ SoftItoOdevHamburgerMenu/Scripts/Js/MainJs.js
@@ -0,0 +1,26 @@
+﻿
+<script>
+    function openNav() {
+        document.getElementById("YanMenu").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "50px";
+    document.getElementById("Ac").style.display = "none";
+    document.getElementById("Kapa").style.display = "none";
+
+
+        }
+
+    function closeNav() {
+        document.getElementById("YanMenu").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("Kapa").style.display = "none";
+    document.getElementById("Ac").style.display = "block";
+        }
+
+    function closeNavInside() {
+        document.getElementById("YanMenu").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("Kapa").style.display = "none";
+    document.getElementById("Ac").style.display = "block";
+        }
+</script>
+
